0420f3133b388329879246e71beabeeb42fc14b2
jorvixsky <jorvixsky@proton.me>
2024-02-05 16:58:32 +0100

    Burner now working (receives tokens to prevent ERC20 preventing sending to burn address)

--- packages/hardhat/contracts/Burner.sol
+++ packages/hardhat/contracts/Burner.sol
@@ -4,16 +4,16 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Burner {
 
-    function burnERC20Token(address token, uint256 amount) public {
+    function burnERC20Token(address _sender, address _token, uint256 _amount) public {
         // Burn tokens
-        IERC20(token).transferFrom(msg.sender, address(0), amount);
+        IERC20(_token).transferFrom(_sender, address(this), _amount);
     }
 
-    function burnERC20Tokens(address[] calldata tokens, uint256[] calldata amounts) public {
+    function burnERC20Tokens(address[] calldata _tokens, uint256[] calldata _amounts) public {
         // Burn tokens
-        for (uint256 i = 0; i < tokens.length; i++) {
+        for (uint256 i = 0; i < _tokens.length; i++) {
             // Burn tokens
-            burnERC20Token(tokens[i], amounts[i]);
+            burnERC20Token(msg.sender, _tokens[i], _amounts[i]);
         }
     }
 }
